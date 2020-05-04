8ee7c1f2b4d852717f36db6dbdf04a1fa6fe92c4
kbetschart2 <kennybetschart03@gmail.com>
2020-05-04 09:59:28 +0200

    added HelloWorld

---
+++ HelloWorld/Build/HelloWorld.c
@@ -0,0 +1,20 @@
+#include <avr\io.h>
+#include <Atmega328P.h>
+#define F_CPU 16000000
+#include <avrlib.h>
+#include <util\delay.h>
+
+#include <inttypes.h>
+
+int main(void)
+{
+	Usart_Init(250000);
+
+	while (True)
+	{
+		Usart_Trace0(1);
+		_delay_ms(1000);
+	}
+	
+	return 0;
+}
---
+++ HelloWorld/Build/HelloWorld.d
@@ -0,0 +1,51 @@
+C:\Projekte\Avr\HelloWorld\Build\HelloWorld.d \
+ C:\Projekte\Avr\HelloWorld\Build\HelloWorld.o: \
+ C:\Projekte\Avr\HelloWorld\Build\HelloWorld.c \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h \
+ C:\Projekte\Avr\ToolChain\AvrLib\include/Atmega328P.h \
+ C:\Projekte\Avr\ToolChain\AvrLib\include/avrlib.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h \
+ c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h:
+
+C:\Projekte\Avr\ToolChain\AvrLib\include/Atmega328P.h:
+
+C:\Projekte\Avr\ToolChain\AvrLib\include/avrlib.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h:
+
+c:\projekte\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h:
